7fc8d8f474cbe30b6367a281872a89cf3b333f7b
Tomasz <tom.jopek@gmail.com>
2019-12-06 21:00:33 +0100

    init commit

---
+++ src/main/java/pl/itj/dev/services/reportsapi/ReportsApiApplication.java
@@ -0,0 +1,13 @@
+package pl.itj.dev.services.reportsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportsApiApplication.class, args);
+    }
+
+}
