e4475feaa4d40ac4dd452585008b75b09e4d90f9
Tomasz <tom.jopek@gmail.com>
2020-01-05 20:04:53 +0100

    working xlsx exporter endpoint

--- src/main/java/pl/itj/dev/services/reportsapi/reports/ReportGeneratorService.java
+++ src/main/java/pl/itj/dev/services/reportsapi/reports/ReportGeneratorService.java
@@ -2,15 +2,16 @@ package pl.itj.dev.services.reportsapi.reports;
 
 import lombok.extern.slf4j.Slf4j;
 import net.sf.jasperreports.engine.*;
-import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
 import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.export.OutputStreamExporterOutput;
 import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
-import net.sf.jasperreports.export.SimpleXlsReportConfiguration;
-import org.apache.commons.lang3.NotImplementedException;
+import net.sf.jasperreports.export.SimpleXlsxExporterConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
 import pl.itj.dev.services.reportsapi.reports.config.ReportPropertiesConfig;
 import pl.itj.dev.services.reportsapi.reports.interfaces.ReportService;
 
@@ -18,13 +19,13 @@ import javax.sql.DataSource;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 @Service
+@Lazy
 @Slf4j
 public class ReportGeneratorService implements ReportService {
 
@@ -39,6 +40,9 @@ public class ReportGeneratorService implements ReportService {
 
     @Override
     public Optional<byte[]> generatePDF(String reportKey) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("CUSTOMER_ID", 1L);
 
@@ -48,6 +52,9 @@ public class ReportGeneratorService implements ReportService {
         try {
             JasperReport jasperReport = (JasperReport) JRLoader.loadObject(compiledTemplateFile);
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource.getConnection());
+
+            stopWatch.stop();
+            log.info("Generating pdf report for customer id = 1, took: " + stopWatch.getTotalTimeMillis() + " [ms]");
 
             return Optional.of(JasperExportManager.exportReportToPdf(jasperPrint));
         } catch (JRException | SQLException e) {
@@ -71,11 +78,11 @@ public class ReportGeneratorService implements ReportService {
 
             OutputStreamExporterOutput outputStreamExporterOutput = new SimpleOutputStreamExporterOutput(outputStream);
 
-            JRXlsExporter exporter = new JRXlsExporter();
+            JRXlsxExporter exporter = new JRXlsxExporter();
             exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
             exporter.setExporterOutput(outputStreamExporterOutput);
 
-            SimpleXlsReportConfiguration configuration = prepareXlsxReportProperties();
+            SimpleXlsxExporterConfiguration configuration = prepareXlsxReportProperties();
             exporter.setConfiguration(configuration);
 
             exporter.exportReport();
@@ -87,30 +94,27 @@ public class ReportGeneratorService implements ReportService {
         return Optional.empty();
     }
 
-    private SimpleXlsReportConfiguration prepareXlsxReportProperties() {
-        SimpleXlsReportConfiguration configuration = new SimpleXlsReportConfiguration();
-        configuration.setOnePagePerSheet(false);
-        configuration.setDetectCellType(true);
-        configuration.setCollapseRowSpan(false);
+    private SimpleXlsxExporterConfiguration prepareXlsxReportProperties() {
+        SimpleXlsxExporterConfiguration configuration = new SimpleXlsxExporterConfiguration();
         return configuration;
     }
 
     @Override
     public Optional<byte[]> generateCSV(String reportKey) {
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("CUSTOMER_ID", 1L);
-
-        File file = new File(reportPropertiesConfig.getJrxmlFilesLocation());
-        File compiledTemplateFile = new File(file, reportKey + ".jasper");
-
-        try {
-            JasperReport jasperReport = (JasperReport) JRLoader.loadObject(compiledTemplateFile);
-            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource.getConnection());
-
-            return Optional.of(JasperExportManager.exportReportToPdf(jasperPrint));
-        } catch (JRException | SQLException e) {
-            log.error("Error during PDF report generation.");
-        }
+//        Map<String, Object> parameters = new HashMap<>();
+//        parameters.put("CUSTOMER_ID", 1L);
+//
+//        File file = new File(reportPropertiesConfig.getJrxmlFilesLocation());
+//        File compiledTemplateFile = new File(file, reportKey + ".jasper");
+//
+//        try {
+//            JasperReport jasperReport = (JasperReport) JRLoader.loadObject(compiledTemplateFile);
+//            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource.getConnection());
+//
+//            return Optional.of(JasperExportManager.exportReportToPdf(jasperPrint));
+//        } catch (JRException | SQLException e) {
+//            log.error("Error during PDF report generation.");
+//        }
 
         return Optional.empty();
     }
