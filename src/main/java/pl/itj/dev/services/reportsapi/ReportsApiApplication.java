f0bc58b804f850fe2d8a7dbf826560457337a34c
Tomasz <tom.jopek@gmail.com>
2020-01-06 20:03:24 +0100

    working eureka registry

--- src/main/java/pl/itj/dev/services/reportsapi/ReportsApiApplication.java
+++ src/main/java/pl/itj/dev/services/reportsapi/ReportsApiApplication.java
@@ -3,12 +3,14 @@ package pl.itj.dev.services.reportsapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import pl.itj.dev.services.reportsapi.reports.config.ReportPropertiesConfig;
 
 @SpringBootApplication()
 @EnableJpaRepositories(basePackages = "pl.itj.dev.services.reportsapi.repositories")
 @ConfigurationPropertiesScan(basePackageClasses = {ReportPropertiesConfig.class})
+@EnableEurekaClient
 public class ReportsApiApplication {
 
     public static void main(String[] args) {
